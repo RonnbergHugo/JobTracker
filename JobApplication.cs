781f592e585cfe969cc152051dad160550a685e6
RonnbergHugo <hugo.ronnberg@student.nbi-handelsakademin.se>
2025-10-15 10:35:03 +0200

    Added the required attributes and methods to a new class.

---
+++ JobApplication.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobTracker {
+    public enum ApplicationStatus {
+        Applied,
+        Interview,
+        Offer,
+        Rejected
+    }
+    internal class JobApplication {
+        public string CompanyName;
+        public string PositionTitle;
+        public DateTime ApplicationDate;
+        public ApplicationStatus Status;
+        public DateTime? ResponseDate;
+        public int SalaryExpectation;
+
+        public int GetDaysSinceApplied() {
+            
+        }
+        public string GetSummary() {
+            
+        }
+
+    }
+}
