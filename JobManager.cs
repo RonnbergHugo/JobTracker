2658ddd1da29d36cfeb55128400d89768f722404
RonnbergHugo <hugo.ronnberg@student.nbi-handelsakademin.se>
2025-10-17 16:14:53 +0200

    Added the AddJob method.

--- JobManager.cs
+++ JobManager.cs
@@ -9,7 +9,42 @@ namespace JobTracker {
         List<JobApplication> jobApplications = new List<JobApplication>();
 
         public void AddJob() {
+            string company;
+            string position;
+            int salary;
+            do {
+                Console.Write("Where did you apply? ");
+                company = Console.ReadLine();
+                Console.WriteLine("To which position did you apply? ");
+                Console.Write("What salary do you expect? ");
+                Console.Clear();
+            }
+            while (company == null || company == "");
 
+            do {
+                Console.WriteLine("Where did you apply? ");
+                Console.Write("To which position did you apply? ");
+                position = Console.ReadLine();
+                Console.Write("What salary do you expect? ");
+                Console.Clear();
+            }
+            while (position == null || position == "");
+
+            do {
+                Console.WriteLine("Where did you apply? ");
+                Console.WriteLine("To which position did you apply? ");
+                Console.Write("What salary do you expect? ");
+                int.TryParse(Console.ReadLine(), out salary);
+                Console.Clear();
+            }
+            while (salary <= 0);
+            
+            jobApplications.Add(new JobApplication() {
+                CompanyName = company,
+                PositionTitle = position,
+                SalaryExpectation = salary
+            });
+            Console.WriteLine("Your application has been added.");
         }
 
         public void UpdateStatus() {
