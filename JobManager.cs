f20b143d6f6bba0f8e535502e6d164da9cd34cd3
RonnbergHugo <hugo.ronnberg@student.nbi-handelsakademin.se>
2025-10-17 15:10:19 +0200

    Added the required attribute and method

---
+++ JobManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobTracker {
+    internal class JobManager {
+        List<JobApplication> jobApplications = new List<JobApplication>();
+
+        public void AddJob() {
+
+        }
+
+        public void UpdateStatus() {
+
+        }
+
+        public void ShowAll() {
+
+        }
+
+        public void ShowByStatus() {
+
+        }
+
+        public void ShowStatistics() {
+
+        }
+    }
+}
